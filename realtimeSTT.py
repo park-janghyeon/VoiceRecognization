835feeb584135d6c2b0b4d6302faea5437c52ca5
typingmitake <ehtjsqls@gmail.com>
2023-11-02 05:35:09 +0900

    왕돈

--- realtimeSTT.py
+++ realtimeSTT.py
@@ -1,65 +1,80 @@
 import os
+import time
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud.speech_v1p1beta1 import types
 import pyaudio
 
 # Google STT 인증 정보 설정
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "challenge-395408-6472f6ef3591.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CREDENTIALS_PATH = os.path.join(BASE_DIR, 'challenge-395408-6472f6ef3591.json')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH
+
+count = 0
+MAX_STREAM_MINUTES = 4.5  # 5분 전에 스트림을 재시작합니다.
 
 class StreamAudioToText:
-    def __init__(self, rate=44100, chunk=1024):
-        # 초기화
-        self.rate = rate  # 샘플링 비율 
-        self.chunk = chunk  # 한 번에 읽을 오디오 데이터 크기
-        
-        # Google Cloud Speech Client 설정
+    def __init__(self, rate=44100, chunk=220500):  
+        self.rate = rate  
+        self.chunk = chunk  
         self.client = speech.SpeechClient()
-        
-        # 인식 설정: 한국어, LINEAR16 인코딩, 주어진 샘플링 비율 사용
         self.config = types.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
             sample_rate_hertz=self.rate,
-            language_code='ko-KR' # 언어코드는 한국어
+            language_code='ko-KR'
         )
-        
-        # 스트리밍 인식 설정
         self.streaming_config = types.StreamingRecognitionConfig(
             config=self.config,
-            interim_results=True  # 중간 결과 계속해서 반환
+            interim_results=True
         )
-        
-        # pyaudio를 사용한 오디오 인터페이스 설정
         self.audio_interface = pyaudio.PyAudio()
         self.audio_stream = self.audio_interface.open(
             format=pyaudio.paInt16,
-            channels=1, # 채널 수는 1
-            rate=self.rate, # rate는 44100
+            channels=1,
+            rate=self.rate,
             input=True,
             frames_per_buffer=self.chunk,
         )
+        self.start_time = time.time()
 
     def listen_print_loop(self, responses):
-        # 받은 응답을 처리하고 CLI에 텍스트 표시
         for response in responses:
             if not response.results:
                 continue
             result = response.results[0]
             if not result.alternatives:
                 continue
-            # 변환된 텍스트 추출
+            
             transcript = result.alternatives[0].transcript
-            # 텍스트를 CLI에 출력
-            print(transcript)
+
+            if result.is_final:
+                global count
+                count += 1
+                print(f'{count}:{transcript}')
+                break
 
     def start(self):
-        # 오디오 스트리밍 시작
-        requests = (types.StreamingRecognizeRequest(audio_content=self.audio_stream.read(self.chunk)) for _ in range(0, int(self.rate / self.chunk)))
-        responses = self.client.streaming_recognize(self.streaming_config, requests)
-        self.listen_print_loop(responses)
-        # 오디오 스트림 종료
+        while True: 
+            if time.time() - self.start_time > MAX_STREAM_MINUTES * 60:
+                self.restart_stream()
+                
+            requests = (types.StreamingRecognizeRequest(audio_content=self.audio_stream.read(self.chunk)) for _ in range(1))
+            responses = self.client.streaming_recognize(self.streaming_config, requests)
+            self.listen_print_loop(responses)
+
+    def restart_stream(self):
         self.audio_stream.stop_stream()
         self.audio_stream.close()
         self.audio_interface.terminate()
+        
+        self.audio_interface = pyaudio.PyAudio()
+        self.audio_stream = self.audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=self.rate,
+            input=True,
+            frames_per_buffer=self.chunk,
+        )
+        self.start_time = time.time()
         
 def main():
     print("실시간 STT 시작. 종료하려면 Ctrl+C를 누르세요.")
